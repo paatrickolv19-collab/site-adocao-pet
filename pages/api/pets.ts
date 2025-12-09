bbd6f9e1cce1b400fa558872cc46607e8e6d1ae6
Patrick Oliveira <paatrickolv19@gmail.com>
2025-12-09 15:13:16 -0300

    Update pets.ts

--- pages/api/pets.ts
+++ pages/api/pets.ts
@@ -1,16 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import pool from '../../lib/db';
+import type { NextApiRequest, NextApiResponse } from "next";
+import pool from "../../lib/db";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { type } = req.query;
 
-    let query = 'SELECT * FROM pets';
+    let query = "SELECT * FROM pets ORDER BY id DESC";
     const values: any[] = [];
 
-    // Se tiver filtro ?type=algum-tipo
     if (type) {
-      query += ' WHERE type = $1';
+      query = "SELECT * FROM pets WHERE type = $1 ORDER BY id DESC";
       values.push(String(type));
     }
 
@@ -18,7 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json(result.rows);
   } catch (error) {
-    console.error('Erro ao buscar pets:', error);
-    res.status(500).json({ error: 'Erro ao buscar pets' });
+    console.error("Erro ao buscar pets:", error);
+    res.status(500).json({ error: "Erro ao buscar pets" });
   }
 }
