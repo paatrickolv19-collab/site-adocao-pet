3e442fa6cf82d40a0102efac4e88986c23d581a7
Patrick Oliveira <paatrickolv19@gmail.com>
2025-12-09 15:25:26 -0300

    Update prisma.config.js

--- prisma/prisma.config.js
+++ prisma/prisma.config.js
@@ -2,4 +2,9 @@ import { defineConfig } from '@prisma/internals';
 
 export default defineConfig({
   schema: './schema.prisma',
+  datasource: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
